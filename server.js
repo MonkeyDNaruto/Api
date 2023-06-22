52f3d0ec74c39a30e58e7dc1524d8b6aa931ac2e
NischalShrestha <nischalshrestha324@gmain.com>
2023-06-22 18:34:46 +0545

    heroku added

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.post("/failure", (req, res) => {
     res.redirect("/")
 })
 
-app.listen(3000, (req, res) => {
+app.listen(process.env.PORT || 3000, (req, res) => {
     console.log("Server is listening on port: 3000");
 })
 
