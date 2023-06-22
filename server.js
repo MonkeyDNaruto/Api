7f4ccce137d2ff6c9aab82830bf18b3e8375b51c
NischalShrestha <nischalshrestha324@gmain.com>
2023-06-22 09:59:57 +0545

    api

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const https = require("https");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -10,13 +11,49 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
 
-    console.log(firstName, lastName, email);
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us12.api.mailchimp.com/3.0/lists/08da7ae232";
+
+    const options = {
+        method: "POST",
+        auth: "Nischal:8c23f836a63f444483f8cb3d38269f06-us12"
+    }
+
+    const request = https.request(url, options, (response) => {
+        response.on("data", (data) => {
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+
 })
 
 app.listen(3000, (req, res) => {
     console.log("Server is listening on port: 3000");
 })
+
+// API key
+// 8c23f836a63f444483f8cb3d38269f06-us12
+
+// List id
+// 08da7ae232
